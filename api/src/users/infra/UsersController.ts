17147248a1e81994b8d34a60c341db52e0d8d2da
martin-garciablanco <programadopormartin@gmail.com>
2023-09-19 15:18:34 +0200

    feat: :sparkles: Return 400 error trying to create an user that already exists

--- api/src/users/infra/UsersController.ts
+++ api/src/users/infra/UsersController.ts
@@ -1,5 +1,6 @@
 import { Application, Request, Response } from "express";
 
+import { UserAlreadyExistsError } from "../application/UserAlreadyExistsError";
 import { UserService } from "../application/UserService";
 import { UserRequest } from "./UserRequest";
 
@@ -9,9 +10,18 @@ export const usersEndpoint = (app: Application): void => {
 	});
 
 	app.post("/users", (req: Request, res: Response) => {
-		const userService = new UserService();
-		const createdUserRequest = userService.createUser(req.body as UserRequest);
+		try {
+			const userService = new UserService();
+			const createdUserRequest = userService.createUser(req.body as UserRequest);
 
-		return res.status(201).send(createdUserRequest);
+			return res.status(201).send(createdUserRequest);
+		} catch (error: unknown) {
+			const userAlreadyExistsError = new UserAlreadyExistsError();
+			if (error instanceof Error && error.message === userAlreadyExistsError.message) {
+				return res.status(400).send(userAlreadyExistsError.message);
+			}
+
+			return res.status(500);
+		}
 	});
 };
--- api/test/users/infra/usersController.spec.ts
+++ api/test/users/infra/usersController.spec.ts
@@ -2,28 +2,48 @@ import request from "supertest";
 
 import app from "../../../src/app";
 import { User } from "../../../src/users/domain/User";
+import { UserRepository } from "../../../src/users/domain/UserRepository";
 import { UserInMemoryRepository } from "../../../src/users/infra/UserInMemoryRepository";
 import { UserRequest } from "../../../src/users/infra/UserRequest";
-import { userRequestStub } from "../userFixtures";
+import { userRequestStub, userStub } from "../userFixtures";
 
-const userRequest: UserRequest = userRequestStub;
+const usersRequest: UserRequest = userRequestStub;
 
+let usersRepository: UserRepository;
 describe("UsersController", () => {
+	beforeEach(() => {
+		usersRepository = UserInMemoryRepository.initialize();
+		usersRepository.users.clear();
+	});
 	it("Should create an User in our inMemory impl receiving the data through users post endpoint", async () => {
 		await request(app)
 			.post("/users")
-			.send(userRequest)
+			.send(usersRequest)
 			.expect(201)
 			.then((response) => {
-				expect(response.body).toEqual(userRequest);
+				expect(response.body).toEqual(usersRequest);
 			});
 
-		const usersRepository = UserInMemoryRepository.initialize();
-		const createdUser: User = usersRepository.users.get(userRequest.email) as User;
+		const createdUser: User = usersRepository.users.get(usersRequest.email) as User;
 
-		expect(createdUser.name).toEqual(userRequest.name);
-		expect(createdUser.lastName).toEqual(userRequest.lastName);
-		expect(createdUser.email).toEqual(userRequest.email);
-		expect(createdUser.phoneNumber).toEqual(userRequest.phoneNumber);
+		expect(createdUser.name).toEqual(usersRequest.name);
+		expect(createdUser.lastName).toEqual(usersRequest.lastName);
+		expect(createdUser.email).toEqual(usersRequest.email);
+		expect(createdUser.phoneNumber).toEqual(usersRequest.phoneNumber);
+	});
+
+	it("Should return an 400 status code trying to create an user that already exists through users post endpoint", async () => {
+		const user = userStub;
+		const createdUser: User = usersRepository.create(user).get();
+		const createdUserRequest: UserRequest = {
+			name: createdUser.name,
+			lastName: createdUser.lastName,
+			email: createdUser.email,
+			phoneNumber: createdUser.phoneNumber,
+		};
+
+		await request(app).post("/users").send(createdUserRequest).expect(400);
+
+		expect(usersRepository.users.size).toEqual(1);
 	});
 });
