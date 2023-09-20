773a4e344102a1f95922536bab60f41fceea7e18
martin-garciablanco <programadopormartin@gmail.com>
2023-09-20 19:08:06 +0200

    feat: :sparkles: Create Modal component

---
+++ frontend/src/sections/layout/components/Modal/Modal.tsx
@@ -0,0 +1,22 @@
+import styles from "./Modal.module.scss";
+
+type ModalProps = {
+	children: string | JSX.Element | JSX.Element[];
+	show: boolean;
+	onClose: () => void;
+};
+
+export function Modal(props: ModalProps) {
+	return (
+		<>
+			{props.show && (
+				<div className={styles.modal}>
+					<button className={styles.closeButton} onClick={props.onClose}>
+						x
+					</button>
+					{props.children}
+				</div>
+			)}
+		</>
+	);
+}
