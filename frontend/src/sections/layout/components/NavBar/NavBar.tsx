22b9b6ca70d68f623576bf883209ae39d1af8726
martin-garciablanco <programadopormartin@gmail.com>
2023-09-20 17:37:15 +0200

    feat: :sparkles: Create NavBar component and styles

---
+++ frontend/src/sections/layout/components/NavBar/NavBar.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import styles from "./NavBar.module.scss";
+
+export function NavBar() {
+	return (
+		<header className={styles.header}>
+			<h1 className={styles.title}>Dashboard of users</h1>
+			<div className={styles.menu}>
+				<Link to={"/"} className={styles.menuItem}>
+					Home
+				</Link>
+				<Link to={"/metrics"} className={styles.menuItem}>
+					Metrics
+				</Link>
+			</div>
+		</header>
+	);
+}
