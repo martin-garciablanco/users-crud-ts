18950fb7b697317012b522f863afaf38584617ef
martin-garciablanco <programadopormartin@gmail.com>
2023-09-20 21:29:58 +0200

    refactor: :recycle: Update DOM of UserTable

--- frontend/src/sections/user/components/UsersTable/UsersTable.tsx
+++ frontend/src/sections/user/components/UsersTable/UsersTable.tsx
@@ -1,29 +1,43 @@
 import { User } from "../../../../domain/User";
+import styles from "./UsersTable.module.scss";
 
 const users: Array<User> = [
 	{ email: "javi@mail.com", name: "Javi", lastName: "Lopez", phoneNumber: "666555444" },
 	{ email: "anna@mail.com", name: "Anna", lastName: "Pisuerga", phoneNumber: "888999777" },
 ];
 
-export function UsersTable() {
+interface UsersTableProps {
+	updateUser: (user: User) => void;
+}
+
+export function UsersTable({ updateUser }: UsersTableProps) {
 	return (
 		<table>
-			<tr>
-				<th>Email</th>
-				<th>Name</th>
-				<th>Last name</th>
-				<th>Phone number</th>
-			</tr>
-			{users.map((user) => {
-				return (
-					<tr>
-						<td>{user.email}</td>
-						<td>{user.name}</td>
-						<td>{user.lastName}</td>
-						<td>{user.phoneNumber}</td>
-					</tr>
-				);
-			})}
+			<thead>
+				<tr>
+					<th>Email</th>
+					<th>Name</th>
+					<th>Last name</th>
+					<th>Phone number</th>
+				</tr>
+			</thead>
+			<tbody>
+				{users.map((user) => {
+					return (
+						<tr key={user.email}>
+							<td>{user.email}</td>
+							<td>{user.name}</td>
+							<td>{user.lastName}</td>
+							<td>{user.phoneNumber}</td>
+							<td>
+								<button className={styles.updateUserButton} onClick={() => updateUser(user)}>
+									✏️
+								</button>
+							</td>
+						</tr>
+					);
+				})}
+			</tbody>
 		</table>
 	);
 }
