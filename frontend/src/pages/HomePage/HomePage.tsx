c4bbb709a85cc12de3228235b19baae2c45bb5cc
martin-garciablanco <programadopormartin@gmail.com>
2023-09-20 18:41:08 +0200

    refactor: :recycle: Replace UserList and Card by UserTable

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,15 @@
 import { NavBar } from "../../sections/layout/components/NavBar/NavBar";
-import { UsersList } from "../../sections/user/components/UsersList/UsersList";
+import { UsersTable } from "../../sections/user/components/UsersTable/UsersTable";
+import styles from "./HomePage.module.scss";
 
 export function HomePage() {
 	return (
 		<>
 			<NavBar />
-			<h2>Current users</h2>
-			<UsersList />
+			<main className={styles.main}>
+				<h2>Current users</h2>
+				<UsersTable />
+			</main>
 		</>
 	);
 }
