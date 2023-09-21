c7bca860e2c9b9cf49bc1d5a95b988419c620486
martin-garciablanco <programadopormartin@gmail.com>
2023-09-21 14:21:18 +0200

    feat(frontend): :sparkles: Make HomePage use redux

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,51 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { useNavigate } from "react-router";
 import { Modal } from "../../components/layout/components/Modal/Modal";
+import { ModalConfirmation } from "../../components/layout/components/ModalConfirmation/ModalConfirmation";
 import { NavBar } from "../../components/layout/components/NavBar/NavBar";
 import { CreateUser } from "../../components/user/CreateUser/CreateUser";
 import { UpdateUser } from "../../components/user/UpdateUser/UpdateUser";
 import { UsersTable } from "../../components/user/UsersTable/UsersTable";
 import { User } from "../../domain/User";
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { createUser, getAllUsers, removeUser, setUserDetails, updateUser } from "../../store/users/usersSlice";
 import styles from "./HomePage.module.scss";
-
-const users: Array<User> = [
-	{ email: "javi@mail.com", name: "Javi", lastName: "Lopez", phoneNumber: "666555444" },
-	{ email: "anna@mail.com", name: "Anna", lastName: "Pisuerga", phoneNumber: "888999777" },
-];
 
 export function HomePage() {
 	const [showUpdateUserModal, setShowUpdateUserModal] = useState(false);
 	const [showCreateUserModal, setShowCreateUserModal] = useState(false);
+	const [showRemoveUserModal, setShowRemoveUserModal] = useState(false);
 	const [userToUpdate, setUserToUpdate] = useState<User>({} as User);
+	const users = useAppSelector((state) => state.users.users);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 
-	const onCloseUpdateModal = () => {
+	useEffect(() => {
+		dispatch(getAllUsers());
+	}, [dispatch]);
+
+	const closeUpdateModal = () => {
 		setShowUpdateUserModal(false);
 	};
 
-	const onCloseCreateModal = () => {
+	const closeCreateModal = () => {
 		setShowCreateUserModal(false);
 	};
 
-	const onUpdateUser = () => {
-		// eslint-disable-next-line no-console, @typescript-eslint/restrict-template-expressions
-		console.log(`do things for updating user: ${userToUpdate}`);
-		setShowUpdateUserModal(false);
-	};
-
-	const onCreateUser = () => {
-		// eslint-disable-next-line no-console, @typescript-eslint/restrict-template-expressions
-		console.log(`do things for creating an user: ${userToUpdate}`);
-		setShowCreateUserModal(false);
-	};
+	const closeRemoveUserModal = () => {
+		setShowRemoveUserModal(false);
+	}
 
 	const openUpdateUserModal = (user: User) => {
 		setUserToUpdate(user);
 		setShowUpdateUserModal(true);
 	};
 
-	const onOpenCreateUserModal = () => {
+	const openCreateUserModal = () => {
 		setUserToUpdate({} as User);
 		setShowCreateUserModal(true);
+	};
+
+	const removeUserModal = (user: User) => {
+		setUserToUpdate(user);
+		setShowRemoveUserModal(true);
+	}
+
+	const modifyUser = async () => {
+		await dispatch(updateUser(userToUpdate));
+		await dispatch(getAllUsers());
+		setShowUpdateUserModal(false);
+	};
+
+	const createNewUser = async () => {
+		await dispatch(createUser(userToUpdate));
+		await dispatch(getAllUsers());
+		setShowCreateUserModal(false);
+	};
+
+	const deleteUser = async () => {
+		await dispatch(removeUser(userToUpdate));
+		await dispatch(getAllUsers());
+		closeRemoveUserModal();
+	}
+
+	const seeUserDetails = (user: User) => {
+		dispatch(setUserDetails(user));
+		navigate(`/users?email=${user.email}`);
 	};
 
 	return (
@@ -54,27 +81,35 @@ export function HomePage() {
 			<main className={styles.main}>
 				<div className={styles.homeHeader}>
 					<h2>Current users</h2>
-					<button className={styles.newUserButton} onClick={onOpenCreateUserModal}>
+					<button className={styles.newUserButton} onClick={openCreateUserModal}>
 						Create User
 					</button>
 				</div>
-				<UsersTable users={users} updateUser={openUpdateUserModal} enableSeeDetails={true} />
+				<UsersTable users={users} update={openUpdateUserModal} remove={removeUserModal} seeDetails={seeUserDetails} enableSeeDetails={true} />
 
-				<Modal show={showUpdateUserModal} onClose={onCloseUpdateModal}>
+				<Modal show={showUpdateUserModal} onClose={closeUpdateModal}>
 					<UpdateUser
 						userToUpdate={userToUpdate}
 						setUserToTupdate={setUserToUpdate}
-						onCancel={onCloseUpdateModal}
-						onUpdate={onUpdateUser}
+						onCancel={closeUpdateModal}
+						onUpdate={modifyUser}
 					/>
 				</Modal>
-				<Modal show={showCreateUserModal} onClose={onCloseCreateModal}>
+				<Modal show={showCreateUserModal} onClose={closeCreateModal}>
 					<CreateUser
 						userToUpdate={userToUpdate}
 						setUserToTupdate={setUserToUpdate}
-						onCancel={onCloseCreateModal}
-						onCreate={onCreateUser}
+						onCancel={closeCreateModal}
+						onCreate={createNewUser}
 					/>
+				</Modal>
+				<Modal show={showRemoveUserModal} onClose={closeRemoveUserModal}>
+					<ModalConfirmation
+					onAccept={deleteUser}
+					onCancel={closeRemoveUserModal}
+					>
+						{`Do you want to permanetly delete ${userToUpdate.email}`}
+					</ModalConfirmation>
 				</Modal>
 			</main>
 		</>
